2ce322769ef2271428cd15143bdaa3bfc887fedd
marina <marina.pacher.silva@escola.pr.gov.br>
2026-02-20 08:08:55 -0300

    Update aula1.py

---
+++ aula1.py
@@ -0,0 +1,8 @@
+print("Olá Mundo")
+nome="Marina"
+idade=16
+minha_altura=1.73
+# usar o print com variaveis:
+#print ("Olá, meu nome é", nome, ", minha idade é", idade)
+#F-Strings
+print(f'Olá meu nome é {nome}, minha idade é {idade} e tenho {alura}m de altura')
